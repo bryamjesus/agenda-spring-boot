57d234f585a58d65c3301b495c5aed7c75ed85fb
Bryam Talledo <bryamtalledog@gmail.com>
2022-08-18 11:02:56 -0500

    Iniciando proyecto

---
+++ src/main/java/com/bryamdev/demo_agenda/DemoAgendaApplication.java
@@ -0,0 +1,13 @@
+package com.bryamdev.demo_agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAgendaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAgendaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bryamdev/demo_agenda/DemoAgendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryamdev.demo_agenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAgendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
